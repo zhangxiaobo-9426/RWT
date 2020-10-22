32eca6ab644fe5e750494c2e4f246fb23864d84d
2237986476@qq.com <2237986476@qq.com>
2020-10-22 15:53:47 +0800

    add my page

--- app/src/main/java/com/example/rwt/MainActivity.java
+++ app/src/main/java/com/example/rwt/MainActivity.java
@@ -6,20 +6,41 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.listener.OnBannerListener;
+import com.youth.banner.loader.ImageLoader;
+import com.youth.banner.loader.ImageLoaderInterface;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static class ImageLoadBanner extends ImageLoader{
 
+        @Override
+        public void displayImage(Context context, Object path, ImageView imageView) {
+            imageView.setImageResource(Integer.parseInt(path.toString()));
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this,R.id.fragment);
-        AppBarConfiguration configuration = new AppBarConfiguration.Builder(bottomNavigationView.getMenu()).build();
-        NavigationUI.setupActionBarWithNavController(this,navController,configuration);
+//       /AppBarConfiguration configuration = new AppBarConfiguration.Builder(bottomNavigationView.getMenu()).build();
+//        NavigationUI.setupActionBarWithNavController(this,navController,configuration);
         NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
     }
+
 }
--- app/src/main/java/com/example/rwt/ui/my/MyFragment.java
+++ app/src/main/java/com/example/rwt/ui/my/MyFragment.java
@@ -12,7 +12,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.rwt.MainActivity;
 import com.example.rwt.R;
+import com.youth.banner.Banner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyFragment extends Fragment {
 
@@ -33,6 +38,21 @@ public class MyFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(MyViewModel.class);
         // TODO: Use the ViewModel
+        initView();
     }
+    public void initView(){
+        Banner banner = null;
+        banner = getView().findViewById(R.id.my_banner);
+        //放图片地址的集合
+        List<Integer> list_path = new ArrayList<>();
+        list_path.add(R.drawable.my_advertise);
+        list_path.add(R.drawable.my_advertise);
 
+        banner.setImageLoader(new MainActivity.ImageLoadBanner());   //设置图片加载器
+        banner.setImages(list_path);  //设置banner中显示图片
+        //设置轮播间隔时间
+        banner.setDelayTime(3000).isAutoPlay(true).start();
+        //设置是否为自动轮播，默认是“是”。
+        //设置完毕后调用
+    }
 }
