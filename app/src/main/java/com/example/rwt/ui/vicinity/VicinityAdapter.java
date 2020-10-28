42856c0c2610d0c355a9cbcc0477bd3b1461dbdc
15507 <1550751448@qq.com>
2020-10-28 22:38:40 +0800

    调整代码

--- app/src/main/java/com/example/rwt/ui/vicinity/VicinityAdapter.java
+++ app/src/main/java/com/example/rwt/ui/vicinity/VicinityAdapter.java
@@ -1,5 +1,7 @@
 package com.example.rwt.ui.vicinity;
 
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.viewholder.BaseViewHolder;
 import com.example.rwt.R;
@@ -14,10 +16,12 @@ public class VicinityAdapter extends BaseQuickAdapter<VicinityCar, BaseViewHolde
 
     @Override
     protected void convert(@NotNull BaseViewHolder holder, VicinityCar vicinityCar) {
-        holder.setText(R.id.vicinity_item_carcolor, vicinityCar.getCarcolor_url())
-                .setText(R.id.vicinity_item_title, vicinityCar.getTitle())
+        //加载图片
+        Glide.with(holder.itemView).load(vicinityCar.getCarcolor_url()).into((ImageView) holder.getView(R.id.vicinity_item_carcolor));
+        holder.setText(R.id.vicinity_item_title, vicinityCar.getTitle())
                 .setText(R.id.vicinity_item_distance, vicinityCar.getDistance())
                 .setText(R.id.vicinity_item_time, vicinityCar.getTime())
                 .setText(R.id.vicinity_item_location, vicinityCar.getLocation());
     }
+
 }
