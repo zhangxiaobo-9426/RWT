8b0f9a587829a3019b3b6b8916b2e12b06df1b76
15507 <1550751448@qq.com>
2020-11-17 22:59:14 +0800

    add navigationfragment

--- app/src/main/java/com/example/rwt/ui/reservation/reservation_order/reservation_pay/unpaid/payment_successful/PaymentSuccessfulFragment.java
+++ app/src/main/java/com/example/rwt/ui/reservation/reservation_order/reservation_pay/unpaid/payment_successful/PaymentSuccessfulFragment.java
@@ -3,11 +3,15 @@ package com.example.rwt.ui.reservation.reservation_order.reservation_pay.unpaid.
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.rwt.R;
 
@@ -17,6 +21,7 @@ import com.example.rwt.R;
 public class PaymentSuccessfulFragment extends Fragment {
 
 
+     private Button button_payment_successful_navigation;
     public PaymentSuccessfulFragment() {
         // Required empty public constructor
     }
@@ -29,4 +34,15 @@ public class PaymentSuccessfulFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_payment_successful, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        button_payment_successful_navigation = view.findViewById(R.id.button_payment_successful_navigation);
+        button_payment_successful_navigation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_paymentSuccessfulFragment_to_navigationFragment);
+            }
+        });
+    }
 }
